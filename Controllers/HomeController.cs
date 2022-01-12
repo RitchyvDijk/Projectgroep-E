df8fe75c92308d3e59ab3b2471cd1ad0aa7f1e21
tomdamo <20182376@student.hhs.nl>
2022-01-12 13:44:35 +0100

    Met intakegesprek
    De view heet intakeVragen

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,6 +30,10 @@ namespace webapplication.Controllers
         {
             return View();
         }
+        public IActionResult intakeVragen()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
