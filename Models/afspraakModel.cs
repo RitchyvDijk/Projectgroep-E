d290abda0be081f56604652a882bbfb8fe123842
tomdamo <20182376@student.hhs.nl>
2022-01-20 14:14:26 +0100

    registratie pt2
    om te registreren en afspraak te maken en al moet je naar de view afspraak/create gaan.

--- Models/afspraakModel.cs
+++ Models/afspraakModel.cs
@@ -7,22 +7,18 @@ public class afspraakModel
     public int id { get; set; }
     public string voornaam { get; set; }
     public string achternaam { get; set; }
-
     //jongerDan16 true = emailvanOuder required
-    public bool jongerDan16{get;set;}
+    public bool jongerDan16 { get; set; }
 
     //gegevens voor de API
     public DateTime geboorteDatum { get; set; }
     public string BSN { get; set; }
-
+    public string naamOuder { get; set; }
     public string emailvanOuder { get; set; }
     public string emailvanGebruiker { get; set; }
-
-    
-    public DateTime gekozenDatum {get;set;}
+    public DateTime gekozenDatum { get; set; }
     //tijd kan 8:00 tot 17:00 zijn in string formaat.
+    public string gekozenTijd { get; set; }
 
-    public string gekozenTijd {get;set;}
-
-    public HulpverlenerModel gekozenHulpverlener { get; set; }
+    public string gekozenHulpverlener { get; set; }
 }
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,8 +31,9 @@ namespace webapplication
 
             services.AddRazorPages();
 
-
-
+            services.AddDbContext<afspraakDbContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("afspraakDbContext")));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
